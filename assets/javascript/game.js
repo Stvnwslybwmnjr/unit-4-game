7f38c9ad5d00f2b435792fb6992beb78800bc6cc
Steven Bowman <stevenbowmanjr@gmail.com>
2019-09-06 14:10:32 -0700

    psuedo coded outline of main events of the game in Javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+// NEEDED VARIABLES================
+
+
+// NEEDED FUNCTIONS================
+
+/* Action 1 - Select Character(object)
+
+    Action 2 - Select Opponent
+
+    Action 3 - Battle
+
+    Action 4 - Select Opponent
+
+    Action 5 - Battle
+
+    Action 6 - Select Opponent
+
+    Action 7 - Battle
+
+    Action 8 - End Game/Reset
+
+*/ 
+
+
+
+// GAME PLAY=======================

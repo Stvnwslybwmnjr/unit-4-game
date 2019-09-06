8074f27c78fa0777afa2a0fc6e580c6837986a0b
Steven Bowman <stevenbowmanjr@gmail.com>
2019-09-06 14:35:25 -0700

    psuedo coded key events in javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,26 @@
 // NEEDED VARIABLES================
+/*  player = the person playing game
+    computer = the current opponent
+
+    4 characters = objects containing properties of healthpoints HP, attackpower AP, counterattackpower CAP,
+    */
 
 
 // NEEDED FUNCTIONS================
 
 /* Action 1 - Select Character(object)
+        a.- click select character assigning that character (object) = player
+        b.- remaing characters move to middle row
 
     Action 2 - Select Opponent
+        a.- click select character assigning that character (object) = computer
+        b.- opponent moves to bottom row
 
     Action 3 - Battle
+        a. attack button click begins battle
+        b. computer HP - player AP
+        c. player HP - computer CAP
+        d. If player HP <= 0 GAMEOVER  elseif computer HP <= 0 disapear opponent
 
     Action 4 - Select Opponent
 
@@ -18,6 +31,7 @@
     Action 7 - Battle
 
     Action 8 - End Game/Reset
+        a. if computer HP <= 0 && no remaining characters exist gameover-reset game
 
 */ 
 
